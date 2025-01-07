cb88e346f6fe09b16c6ea4d7c5e2a968df37d12e
SerUKalinin <serukalinin@gmail.com>
2025-01-07 19:55:46 +0300

    first commit

---
+++ src/main/java/com/example/real_estate_app_account/RealEstateAppAccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_estate_app_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAppAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstateAppAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real_estate_app_account/RealEstateAppAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_estate_app_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAppAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
